9974059f693c1ebdd7b160bd4959cde1ee232fe3
duz111 <88191166+duz111@users.noreply.github.com>
2024-05-14 11:31:01 +0800

    Delete serialtest.py

--- serialtest.py
+++
@@ -1,5 +0,0 @@
-import serial
-ser = serial.Serial('COM1')  # open serial port
-print(ser.name)         # check which port was really used
-ser.write(b'hello')     # write a string
-ser.close()      
